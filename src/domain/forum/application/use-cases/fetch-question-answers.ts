965be43153c41639fd7b9bf68d5e221b31ed8119
JorgeWS <jorgews.dev@gmail.com>
2024-10-11 07:27:20 -0300

    feat: 🎸 Fetch Question Answer Use Case

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,29 @@
+import { AnswerRepository } from "@/domain/forum/application/repositories/answer-repository";
+import { Answer } from "../../enterprise/entities/answer";
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string;
+  page: number;
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[];
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answerRepository: AnswerRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answerRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    );
+
+    return {
+      answers,
+    };
+  }
+}
--- src/test/repositories/in-memory-answer-repository.ts
+++ src/test/repositories/in-memory-answer-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerRepository } from "@/domain/forum/application/repositories/answer-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
@@ -14,6 +15,17 @@ export class InMemoryAnswerRepository implements AnswerRepository {
     if (!answer) return null;
 
     return answer;
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answers;
   }
 
   async delete(answer: Answer): Promise<void> {
