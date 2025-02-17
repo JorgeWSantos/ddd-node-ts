9775bfe017ef3c88095c70c753a5bb294f38e458
JorgeWS <jorgews.dev@gmail.com>
2025-02-17 06:57:44 -0300

    feat: 🎸 Add either class to handle requisitions

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from "./either";
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right("success");
+  }
+
+  return left("error");
+}
+
+test("success", () => {
+  const sucessResult = doSomething(true);
+
+  if (sucessResult.isRight()) {
+    console.log(sucessResult.value);
+  }
+
+  expect(sucessResult.isRight()).toBe(true);
+  expect(sucessResult.isLeft()).toBe(false);
+});
+
+test("error", () => {
+  const errorResult = doSomething(false);
+
+  expect(errorResult.isLeft()).toBe(true);
+  expect(errorResult.isRight()).toBe(false);
+});
